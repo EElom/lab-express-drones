6f64c87d25898770bb4fe00fbfc90a6adcd20d71
Elom ESSAN <elomessan@Eloms-iMac.local>
2022-05-05 20:38:50 +0200

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,22 @@
 // Iteration #1
+const { Schema, model } = require('mongoose');
+
+const droneSchema = new Schema[{
+  name: {
+    type: String,
+    required: true
+  },
+  propellers: {
+    type: Number,
+    required: true
+  },
+  maxSpeed: {
+    type: Number,
+    default: 0
+  },
+  
+}]
+
+const Drone = model('Drone', droneSchema);
+
+module.exports = Drone;
